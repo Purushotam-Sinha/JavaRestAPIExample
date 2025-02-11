c906cbe64e92e973a458c912c396aa082f84a9c7
Purushotam-Sinha <37238038+Purushotam-Sinha@users.noreply.github.com>
2025-02-11 15:36:35 +0530

    Update RestControllerExample.java
    New endpoints added

--- src/main/java/com/example/demo/RestControllerExample.java
+++ src/main/java/com/example/demo/RestControllerExample.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import java.util.Date;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +17,12 @@ public class RestControllerExample {
 		System.out.println("Rest ApI called ");
 		return "<h2><font color='green'>Welcome Mr ."+new Date()+"</font></h2>";
 	}
-
+	
+	
+       @GetMapping("/{name}")
+	public String getName(@PathVariable String name)
+	{
+		System.out.println("Rest ApI called ");
+		return "<h2><font color='green'>Welcome Mr ."+name+"</font></h2>";
+	}
 }
